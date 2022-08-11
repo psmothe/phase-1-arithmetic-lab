d895a4d226c6c0d4d2bbb8ec477e9d73fc50efe0
psmothe <108036228+psmothe@users.noreply.github.com>
2022-08-10 19:17:53 -0500

    commit phase-1-arithmetic-lab passing tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()) + 2;
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
